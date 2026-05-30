de7c2030f9f4c205e4ce910b1c8d6910c97f4e64
melanimelalunati24-boop <melani.melaluna_ti24@nusaputra.ac.id>
2026-05-30 21:19:57 +0700

    Create person.java

---
+++ person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String name;
+    private String address;
+
+    // Constructor
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Format output sesuai petunjuk UML: "name(address)"
+    @Override
+    public String toString() {
+        return name + "(" + address + ")";
+    }
+}
